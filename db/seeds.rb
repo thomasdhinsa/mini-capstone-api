2ec6fc7119b6972d3db26d80ca1c4034204c6d55
Tom Dhinsa <mrdhinsa05@gmail.com>
2022-01-12 12:49:03 -0500

    adds burger to products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Product.create(name: "burger", price: 5, description:"100% grass-fed beef")
