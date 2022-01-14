8a3894fc68f196a8ecfa297dfecf90f29868135d
Tom Dhinsa <mrdhinsa05@gmail.com>
2022-01-14 16:16:42 -0500

    adds REST syntax for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
- 
-  get "/specific_query/:id" => "products#specific_query"
-  post "/create/" => "products#create"
-  get "/show/:id" => "products#show"
-  get "/index" => "products#index"
-  patch "/update/:id" => "products#update"
-  delete "destroy/:id" => "products#destroy"
-
-
-
+  post "/products/" => "products#create"
+  get "/products/:id" => "products#show"
+  get "/products/" => "products#index"
+  patch "/products/:id" => "products#update"
+  delete "/products/:id" => "products#destroy"
 
 
 
