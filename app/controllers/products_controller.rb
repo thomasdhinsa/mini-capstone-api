8da32a4b95d14f1eec5119d352ff71fc40be01bc
Tom Dhinsa <mrdhinsa05@gmail.com>
2022-01-14 15:25:10 -0500

    adds working REST methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,44 @@
 class ProductsController < ApplicationController
+  def create
+    product = Product.new(
+
+    name: params[:name],
+    price: params[:price],
+    image_url: params[:image_url],
+    description: params[:description]
+
+    )
+    product.save
+    render json: product.as_json
+  end 
 
 
-#def display_product
-  #product = Product.all
-  #render json: Product.as_json 
-#end 
-   
+  def show
+    id = params[:id].to_i
+    product = Product.find_by(id: id)
+  render json: product.as_json 
+  end 
+     
+  def index
+    product = Product.all
+    render json: product.as_json 
+  end 
 
-def display_query
-  render json: {message: "Am I insane?"}
-  end
+  def update 
+     product = Product.find(params[:id])
+     product.name = params[:name] || product.name
+     product.price = params[:price] || product.price
+     product.image_url = params[:image_url] || product.image_url
+     product.save
+   #product.update(symbol: params[symbol:])(for use in console)
+     render json: product.as_json 
+   end 
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    render json: {message: "Product has been deleted"}
+  end 
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
  
-  #get "/display_product",controller:"products",action:"display_product"
-  #get "/display_product" => "products#display_product"
+  get "/specific_query/:id" => "products#specific_query"
+  post "/create/" => "products#create"
+  get "/show/:id" => "products#show"
+  get "/index" => "products#index"
+  patch "/update/:id" => "products#update"
+  delete "destroy/:id" => "products#destroy"
 
-  get "/display_query/:food_stuff" => "products#display_query" #works
+
+
+
+
+
 end
